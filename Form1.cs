0aaf78579668eb68ebe28b5079372b176a7910b9
Jesús Lagares <48330849+Lagaress@users.noreply.github.com>
2022-07-16 16:56:16 +0200

    Project pivoted to CSharp

---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlpacaDesktop
+{
+    public partial class Form1 : Form
+    {
+        private bool isDraggable = false;
+        private Point point = new Point(0, 0);
+        private Timer timer;
+        public Form1()
+        {
+            InitializeComponent();
+            this.BackColor = Color.LimeGreen;
+            this.TransparencyKey = Color.LimeGreen;
+            TopMost = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            initTimer();
+        }
+        private void initTimer()
+        {
+            timer = new Timer();
+            timer.Tick += new EventHandler(displayRandomNumber);
+            timer.Interval = 5000; 
+            timer.Start();
+        }
+
+        private void displayRandomNumber(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(1, 5);
+
+            MessageBox.Show(randomNumber.ToString());
+        }
+
+        private void pictureBox2_DoubleClick(object sender, EventArgs e)
+        {
+            MessageBox.Show("AQUÍ LA ALPACA CAMBIA DE COLOR");
+        }
+        private void pictureBox2_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDraggable = false;
+        }
+
+        private void pictureBox2_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDraggable = true;
+            point = new Point(e.X, e.Y);
+        }
+
+        private void pictureBox2_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDraggable)
+            {
+                Point pointToScreen = PointToScreen(e.Location);
+                Location = new Point(pointToScreen.X - this.point.X, pointToScreen.Y - this.point.Y);
+            }
+        }
+    }
+}
